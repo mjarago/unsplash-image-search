b01fbe1aa7fd0535db9ffb767e425d42218d5e27
Mark Arago <markjosepharago@gmail.com>
2021-01-03 13:22:07 -0800

    Search Function added

--- app/src/main/java/com/markarago/unsplashimagesearch/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/markarago/unsplashimagesearch/ui/gallery/GalleryFragment.kt
@@ -1,13 +1,19 @@
 package com.markarago.unsplashimagesearch.ui.gallery
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
 import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
+import androidx.paging.LoadState
 import com.markarago.unsplashimagesearch.R
 import com.markarago.unsplashimagesearch.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_gallery.*
 
 @AndroidEntryPoint
 class GalleryFragment: Fragment(R.layout.fragment_gallery) {
@@ -27,18 +33,68 @@ class GalleryFragment: Fragment(R.layout.fragment_gallery) {
 
         binding.apply {
             recyclerView.setHasFixedSize(true)
+            recyclerView.itemAnimator = null
             recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
                 header = UnsplashPhotoLoadStateAdapter { adapter.retry() },
                 footer = UnsplashPhotoLoadStateAdapter { adapter.retry() },
             )
+            buttonRetry.setOnClickListener {
+                adapter.retry()
+            }
+
         }
 
         viewModel.photos.observe(viewLifecycleOwner) {
             // when value of photos changes
             // connect data to adapter
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
-
         }
+
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                progress_bar.isVisible = loadState.source.refresh is LoadState.Loading
+                recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
+                buttonRetry.isVisible = loadState.source.refresh is LoadState.Error // no internet connection
+                textViewError.isVisible = loadState.source.refresh is LoadState.Error // no internet connection
+
+                // empty view
+                if (loadState.source.refresh is LoadState.Loading &&
+                        loadState.append.endOfPaginationReached &&// no more results to be loaded
+                        adapter.itemCount < 1) { // no data
+                    recyclerView.isVisible = false
+                    textViewEmpty.isVisible = true
+                } else {
+                    textViewEmpty.isVisible = false
+                }
+
+            }
+        }
+
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+
+        inflater.inflate(R.menu.menu_gallery, menu)
+
+        val searchItem = menu.findItem(R.id.action_search)
+        val searchView = searchItem.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                if (query != null) {
+                    binding.recyclerView.scrollToPosition(0) // jump to top on search
+                    viewModel.searchPhotos(query)
+                    searchView.clearFocus() // hide keyboard
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+               return true
+            }
+        })
     }
 
     override fun onDestroyView() {
